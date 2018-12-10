c000aa2e0c098c265bacb9677d9b9962b213154e
hmunguba <hmunguba@gmail.com>
2018-12-09 20:57:54 -0400

    Add Firebase Realtime Database dependencies

--- app/src/main/java/com/community/hmunguba/condominium/view/ui/MenuActivity.java
+++ app/src/main/java/com/community/hmunguba/condominium/view/ui/MenuActivity.java
@@ -1,13 +1,21 @@
 package com.community.hmunguba.condominium.view.ui;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.community.hmunguba.condominium.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuActivity extends AppCompatActivity {
+    private static final String TAG = MenuActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,24 @@ public class MenuActivity extends AppCompatActivity {
         MenuFragment menuFragment = new MenuFragment();
         ft.add(R.id.menu_container, menuFragment);
         ft.commit();
+
+        // Firebase database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("hello database");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
