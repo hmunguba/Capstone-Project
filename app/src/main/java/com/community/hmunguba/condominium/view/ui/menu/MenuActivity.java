660317e2f70283b9d18c339adbec65eef812d358
hmunguba <hmunguba@gmail.com>
2018-12-24 18:47:14 -0400

    Add logout menu to MenuActivity

--- app/src/main/java/com/community/hmunguba/condominium/view/ui/menu/MenuActivity.java
+++ app/src/main/java/com/community/hmunguba/condominium/view/ui/menu/MenuActivity.java
@@ -1,13 +1,19 @@
 package com.community.hmunguba.condominium.view.ui.menu;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.community.hmunguba.condominium.R;
+import com.community.hmunguba.condominium.service.model.repo.FirebaseUserAuthentication;
+import com.community.hmunguba.condominium.view.ui.login.LoginActivity;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -47,5 +53,29 @@ public class MenuActivity extends AppCompatActivity {
                 Log.w(TAG, "Failed to read value.", databaseError.toException());
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sign_out:
+                FirebaseUserAuthentication.getInstance().signOut();
+                startLoginActivity();
+                return true;
+            default :
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void startLoginActivity() {
+        Intent intent = new Intent(MenuActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
